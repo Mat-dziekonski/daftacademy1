089361d7407ec87cd72d664bb0e8182857d24da2
Mateusz Dziekoński <42418962+Mat-dziekonski@users.noreply.github.com>
2020-04-05 21:26:37 +0200

    Create main.py

---
+++ zadanie2/main.py
@@ -0,0 +1,13 @@
+# main.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def read_item(name: str):
+    return f"Hello {name}"
